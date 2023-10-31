822b1e9faa3b686f471dd57e4c6b95f5ad4aed17
riyaaaa123 <riyajindal769@gmail.com>
2023-10-31 23:06:57 +0530

    [feat] prepared header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,18 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Theme from '@/components/theme'
+import Navbar from '@/components/Navbar'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-   <>
-   <Theme/>
-   </>
-  )
+    <>
+      <div className="bg bg-cover bg-blur h-screen min-w-screen bg-center "
+        style={{ backgroundImage: "url('/bg.png')" }}>
+          <Navbar/>
+
+      </div>
+     
+    </>
+  );
 }
