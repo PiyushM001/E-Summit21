035b74150001576b7aaedfac25a86188b3beda08
piyush <pmimpiyush@gmail.com>
2023-11-02 21:36:46 +0530

    fixed bugs in components

--- components/theme.jsx
+++ components/theme.jsx
@@ -8,10 +8,10 @@ export default function Theme() {
 <p>It provides an avenue to exhibit the entrepreneurial talent and creativity through many events like business ventures, product design competition, and social idea competition.</p>
 <p>E-Summit being the flagship event of ECell, is held annually brings together the academic community, venture capitalists, new age entrepreneurs and all those passionate about entrepreneurship to common grounds.</p>
       </div>
-      <div className='flex items-center justify-center theme-image-div0'>
+      <div data-aos="fade-zoom-in" data-aos-offset="200" data-aos-easing="ease-in-sine" data-aos-duration="600" className='flex items-center justify-center theme-image-div0'>
       <div className='theme-image-div'>
       <img className='theme-img-2' src={'/images/Upper.svg'}></img>
-      <img className='theme-img' src={'/images/Pic.svg'}></img>
+      <img  className='theme-img' src={'/images/Pic.svg'}></img>
       <img className='theme-img-3' src={'/images/Lower.svg'}></img>
 
 
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,10 +3,16 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>       <link rel="stylesheet" href="bower_components/aos/dist/aos.css" />
+</Head> 
+      
       <body>
         <Main />
         <NextScript />
+        <script src="bower_components/aos/dist/aos.js"></script> 
+        <script>
+    AOS.init();
+  </script>
       </body>
     </Html>
   )
