fdb4b8bdb17ec1d5edbdabb3e790ab969cc2cd5e
piyush <pmimpiyush@gmail.com>
2023-11-04 03:15:19 +0530

    added animation in theme and added AOS library

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,19 +1,14 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>       <link rel="stylesheet" href="bower_components/aos/dist/aos.css" />
-</Head> 
-      
+      <Head />
+
       <body>
         <Main />
         <NextScript />
-        <script src="bower_components/aos/dist/aos.js"></script> 
-        <script>
-    AOS.init();
-  </script>
       </body>
     </Html>
-  )
+  );
 }
