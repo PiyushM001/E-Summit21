efdf8f9dc49534e60ddc5a573b7c6aa575e14e1d
riyaaaa123 <riyajindal769@gmail.com>
2023-11-04 23:14:13 +0530

    [feat] integrated landing page with navbar

--- pages/index.js
+++ pages/index.js
@@ -8,12 +8,10 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <>
-      <div className="bg bg-cover bg-blur h-screen min-w-screen bg-center "
-        style={{ backgroundImage: "url('/bg.png')" }}>
+      
         <Navbar/>
 
-      </div>
-      <Footer></Footer>
+        <Footer></Footer>
      
     </>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,19 +2,30 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-    },
+      screens: {
+        xs: { max: "350px" },
+        sm: { max: "600px", min: "350px" },
+        mid: { max: "1000px", min: "600px" },
+        l: { min: "1000px" },
+      },
+      fontFamily: {
+        proxima: ["Proxima Nova", "sans"],
+      },
+     
   },
+},
+
   plugins: [],
-}
+};
 export default config
